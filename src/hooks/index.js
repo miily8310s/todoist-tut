f89f9242e7e154d84a1c755cab9b7fb6a175a805
miily8310s <rillakerika@gmail.com>
2021-04-02 08:52:37 +0900

    add temp change

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-// import moment from 'moment';
+import moment from 'moment';
 import { firebase } from '../firebase';
 import { collatedTasksExist } from '../helpers';
 
